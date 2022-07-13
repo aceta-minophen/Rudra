7a175e060e3565ae58f71846e8c9aa9805ebd75c
Manasvvi <manasvvi.aggarwal@gmail.com>
2022-07-14 01:07:41 +0530

    added appBar of health page

---
+++ companionapp/lib/custom_icons_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData gym_dumbbell = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData daily_schedule = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData message_phone_chat = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData remote_control_line = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
---
+++ companionapp/lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:companionapp/custom_icons_icons.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Health());
+}
+
+class Health extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: Container(
+            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+            child: Image.network('https://static.remove.bg/remove-bg-web/5c20d2ecc9ddb1b6c85540a333ec65e2c616dbbd/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          title: Column(
+            children: [
+              Text('Good Morning, Emilie!', style: TextStyle(fontSize: 15),),
+              Text('30 April, 2022', style: TextStyle(fontSize: 10))
+            ],
+          ),
+          actions: [
+            IconButton(
+                onPressed: (){},
+                icon: Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                )
+            )
+          ],
+        ),
+        bottomNavigationBar: Container(
+          color: Color(0xFF171C23),
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: (){},
+                  icon: Icon(
+                    CustomIcons.gym_dumbbell,
+                    // size: 30,
+                    color: Colors.white,
+                  )
+              )
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
