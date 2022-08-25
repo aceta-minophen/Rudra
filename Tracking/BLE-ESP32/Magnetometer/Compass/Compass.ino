8bb76f4e533bb3cc5c0e3ee0427f125d694282c0
aceta-minophen <87569188+aceta-minophen@users.noreply.github.com>
2022-08-25 11:33:24 +0530

    Update Compass.ino

--- Tracking/BLE-ESP32/Magnetometer/Compass/Compass.ino
+++ Tracking/BLE-ESP32/Magnetometer/Compass/Compass.ino
@@ -1,108 +1,159 @@
 /*
- * 
- * Compass.ino - Example sketch for reading a heading from an HMC5883L triple axis magnetometer.
- * 
- * GY-273 Compass Module  ->  Arduino
- * VCC  -> VCC  (See Note Below)
- * GND  -> GND
- * SCL  -> A5/SCL, (Use Pin 21 on the Arduino Mega)
- * SDA  -> A4/SDA, (Use Pin 20 on the Arduino Mega)
- * DRDY -> Not Connected (in this example)
- * 
- * Voltage Note
- * ~~~~~~~~~~~~  
- * The GY-273 Board has a 3v3 Regulator on it, and the SDA/SCL are pulled up to that so it is OK to 
- * use with 5v Arduino's.
- * 
- * If you are using any other breakout, or the raw IC, you need to be using 3v3 to supply and signal!
- * 
- * Datasheet: http://goo.gl/w1criV
- * 
- * Copyright (C) 2014 James Sleeman
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a 
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
- * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
- * THE SOFTWARE.
- * 
- * @author James Sleeman, http://sparks.gogo.co.nz/
- * @license MIT License
- * 
- */
+   e-Gizmo QMC5883L GY-271 Compass
+
+   Sample sketch for the GY-271 QMC5883L
+   for getting the raw data of x, y, z and
+   Radius in degrees.
+
+   Codes by e-Gizmo Mechatronix Central
+   http://www.e-gizmo.com
+   July 10,2017
+
+*/
+
 
 #include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+
 #include <Wire.h>
-#include <HMC5883L_Simple.h>
+#include <QMC5883L.h>
 
-// Create a compass
-HMC5883L_Simple Compass;
+QMC5883L compass;
 
-void setup()
-{
-  Serial.begin(9600);
+// Insert your network credentials
+#define WIFI_SSID "paya1"
+#define WIFI_PASSWORD "naina123"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyA3sxz8LTLgtvdkvBAaLvZO7gBLqzuLM_A"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://rudra-x-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+int count = 0;
+bool signupOK = false;
+
+
+const int trigPin1 = 15;
+const int echoPin1 = 2;
+
+
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration1;
+float distanceCm1;
+float distanceInch;
+
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(trigPin1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin1, INPUT); // Sets the echoPin as an Input
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp(&config, &auth, "", "")){
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else{
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
   Wire.begin();
-    
-  // Magnetic Declination is the correction applied according to your present location
-  // in order to get True North from Magnetic North, it varies from place to place.
-  // 
-  // The declination for your area can be obtained from http://www.magnetic-declination.com/
-  // Take the "Magnetic Declination" line that it gives you in the information, 
-  //
-  // Examples:
-  //   Christchurch, 23° 35' EAST
-  //   Wellington  , 22° 14' EAST
-  //   Dunedin     , 25° 8'  EAST
-  //   Auckland    , 19° 30' EAST
-  //    
-  Compass.SetDeclination(23, 35, 'E');  
-  
-  // The device can operate in SINGLE (default) or CONTINUOUS mode
-  //   SINGLE simply means that it takes a reading when you request one
-  //   CONTINUOUS means that it is always taking readings
-  // for most purposes, SINGLE is what you want.
-  Compass.SetSamplingMode(COMPASS_CONTINUOUS);
-  
-  // The scale can be adjusted to one of several levels, you can probably leave it at the default.
-  // Essentially this controls how sensitive the device is.
-  //   Options are 088, 130 (default), 190, 250, 400, 470, 560, 810
-  // Specify the option as COMPASS_SCALE_xxx
-  // Lower values are more sensitive, higher values are less sensitive.
-  // The default is probably just fine, it works for me.  If it seems very noisy
-  // (jumping around), incrase the scale to a higher one.
-  Compass.SetScale(COMPASS_SCALE_130);
-  
-  // The compass has 3 axes, but two of them must be close to parallel to the earth's surface to read it, 
-  // (we do not compensate for tilt, that's a complicated thing) - just like a real compass has a floating 
-  // needle you can imagine the digital compass does too.
-  //
-  // To allow you to mount the compass in different ways you can specify the orientation:
-  //   COMPASS_HORIZONTAL_X_NORTH (default), the compass is oriented horizontally, top-side up. when pointing North the X silkscreen arrow will point North
-  //   COMPASS_HORIZONTAL_Y_NORTH, top-side up, Y is the needle,when pointing North the Y silkscreen arrow will point North
-  //   COMPASS_VERTICAL_X_EAST,    vertically mounted (tall) looking at the top side, when facing North the X silkscreen arrow will point East
-  //   COMPASS_VERTICAL_Y_WEST,    vertically mounted (wide) looking at the top side, when facing North the Y silkscreen arrow will point West  
-  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
-  
+  compass.init();
 }
 
-// Our main program loop.
-void loop()
-{
-   float heading = Compass.GetHeadingDegrees();
-   
-   Serial.print("Heading: \t");
-   Serial.println( heading );   
-   delay(1000);
+
+void loop() {
+  int x,y,z;
+  compass.read(&x,&y,&z);
+
+ // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  // Atan2() automatically check the correct formula taking care of the quadrant you are in
+  float heading = atan2(y, x);
+
+  float declinationAngle = 0.0404;
+  heading += declinationAngle;
+  // Find yours here: http://www.magnetic-declination.com/
+
+   // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/M_PI; 
+
+
+  Serial.print("x: ");
+  Serial.print(x);
+  Serial.print("    y: ");
+  Serial.print(y);
+  Serial.print("    z: ");
+  Serial.print(z);
+  Serial.print("    heading: ");
+  Serial.print(heading);
+  Serial.print("    Radius: ");
+  Serial.print(headingDegrees);
+  Serial.println();
+
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 100 || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+    // Write an Int number on the database path test/int
+    if (Firebase.RTDB.setInt(&fbdo, "magnet/dir", headingDegrees)){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+  delay(100);
 }
